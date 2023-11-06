515c86159033cc3c51f5a50c07e7a3f453a61f64
ssrisunt <ssrisunt@hotmail.com>
2023-11-06 09:18:14 -0600

    Change to US-EAST-1

--- Container-Root/eks/deployment/hpc/do-gromacs/Container-Root/setup-run.sh
+++ Container-Root/eks/deployment/hpc/do-gromacs/Container-Root/setup-run.sh
@@ -59,7 +59,7 @@ popd
 aws --version
 
 # Install kubectl
-curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/kubectl
+curl -o kubectl https://amazon-eks.s3.us-east-1.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mv ./kubectl /usr/local/bin
 kubectl version --client
